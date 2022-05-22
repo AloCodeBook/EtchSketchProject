09db24729ae68e6ef71c8da22f38e9fda79d0855
Antoine Lolliot <antoinelolliot@gmail.com>
2022-05-21 19:22:36 -0500

    complete etch board with styling and functionality

--- script.js
+++ script.js
@@ -1,14 +1,61 @@
-function CreateSquareDiv() {
-const container = document.querySelector('#container');
-const squareDiv = document.createElement('div');
-squareDiv.classList.add('squareDiv');
-squareDiv.style.cssText = 'height: 50px; width: 50px; background-color: red;border:1px;margin: 10px;'
-container.appendChild(squareDiv);
-}
-CreateSquareDiv();
+let color = "black";
+let click = true;
 
-for (let index = 0; index < 16; index++) {
-    CreateSquareDiv();
-    
-    
+function populateBoard(size) {
+let board = document.querySelector(".board");
+let squares = board.querySelectorAll('div');
+squares.forEach((div) => div.remove());
+board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+let amount = size * size;
+for (let i = 0; i< amount ; i++) {
+    let square = document.createElement('div');
+    square.addEventListener("mouseover",colorSquare);
+    square.style.backgroundColor = "white";
+    board.insertAdjacentElement('beforeend',square);
 }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    if (input >= 2 && input <=100){
+        document.querySelector('.error').style.display = 'none';
+       populateBoard(input); 
+    } else {
+        document.querySelector('.error').style.display = 'flex';
+        console.log('too many squares');
+    }
+}
+
+function colorSquare() {
+    if (click) {
+    if (color === 'random') {
+        this.style.backgroundColor = "#" + ((1<<24)*Math.random() | 0).toString(16);
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+}
+
+function changeColor(choice) {
+color = choice;
+}
+
+function resetBoard() {
+let board = document.querySelector(".board");
+let squares = board.querySelectorAll('div');
+squares.forEach((div) => div.style.backgroundColor = "white");
+}
+
+document.querySelector('body').addEventListener('click', (e) => {
+   if(e.target.tagName != "BUTTON") {
+    click = !click;
+    if (click) {
+        document.querySelector('.mode').textContent = "Mode: Coloring"
+    } else {
+        document.querySelector('.mode').textContent = "Mode: Not Coloring"
+    }
+}
+}); 
